4b95762cb63f67f5a71abbb71d2e5d845c986807
Michael Meli <mjmeli94@gmail.com>
2021-09-08 09:04:17 -0400

    remove version string in initial log message

--- custom_components/duke_energy_gateway/const.py
+++ custom_components/duke_energy_gateway/const.py
@@ -23,7 +23,6 @@ DEFAULT_NAME = DOMAIN
 STARTUP_MESSAGE = f"""
 -------------------------------------------------------------------
 {NAME}
-Version: {VERSION}
 This is a custom integration!
 If you have any issues with this you need to open an issue here:
 {ISSUE_URL}
